f401284963ce316598bbee88dab5cc1270a7c2bb
Rachel <rachelwilson@wl-214-179.wpa.claremont.edu>
2014-09-05 23:43:06 -0700

    Initial Commit

---
+++ BullsEye/RWViewController.h
@@ -0,0 +1,13 @@
+//
+//  RWViewController.h
+//  BullsEye
+//
+//  Created by Rachel Wilson on 9/5/14.
+//  Copyright (c) 2014 Rachel Wilson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWViewController : UIViewController
+
+@end
---
+++ BullsEye/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BullsEye
+//
+//  Created by Rachel Wilson on 9/5/14.
+//  Copyright (c) 2014 Rachel Wilson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RWAppDelegate class]));
+    }
+}
