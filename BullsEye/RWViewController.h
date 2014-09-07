b5b8440ce753590ec5793019fd18731885a08127
Rachel <rachelwilson@wl-214-179.wpa.claremont.edu>
2014-09-06 22:51:06 -0700

    Fixed bug.  Added a random number as the target value.  Page 60 of the tutorial.

--- BullsEye/RWViewController.h
+++ BullsEye/RWViewController.h
@@ -10,6 +10,8 @@
 
 @interface RWViewController : UIViewController
 
+@property (nonatomic, weak) IBOutlet UISlider *slider;
+
 - (IBAction)showAlert;
 
 - (IBAction)sliderMoved:(UISlider *)slider;
--- BullsEye/RWViewController.m
+++ BullsEye/RWViewController.m
@@ -15,12 +15,14 @@
 @implementation RWViewController
 {
     int _currentValue;
+    int _targetValue;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	_currentValue = self.slider.value;
+    _targetValue = 1 + arc4random_uniform(100);
 }
 
 - (void)didReceiveMemoryWarning
@@ -32,7 +34,7 @@
 - (IBAction)showAlert
 {
     NSString *message = [NSString stringWithFormat:
-        @"The value of the slider is: %d", _currentValue];
+        @"The value of the slider is: %d\nThe target value is: %d", _currentValue, _targetValue];
     
     UIAlertView *alertView = [[UIAlertView alloc]
         initWithTitle:@"Hello, World!"
