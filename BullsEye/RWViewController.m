c5eafac02ed82511482a5195d4d7f91104039a92
Rachel <rachelwilson@wl-214-179.wpa.claremont.edu>
2014-09-09 12:48:35 -0700

    Added support for 4 inch screens.  Added app icon images.

--- BullsEye/RWViewController.m
+++ BullsEye/RWViewController.m
@@ -119,8 +119,15 @@
 
 - (IBAction)startOver
 {
+    CATransition *transition = [CATransition animation];
+    transition.type = kCATransitionFade;
+    transition.duration = 1;
+    transition.timingFunction = [CAMediaTimingFunction
+                                 functionWithName:kCAMediaTimingFunctionEaseOut];
+    
     [self startNewGame];
     [self updateLabels];
+    [self.view.layer addAnimation:transition forKey:nil];
 }
 
 - (void)startNewGame
